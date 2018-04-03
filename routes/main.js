0cd5ae7c4c015f7f554ebeb19ec2586e79fde861
ennovatenow <anish.thomas@evolvussolutions.com>
2018-04-03 16:18:11 +0530

    handlebars working

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,9 @@
-const debug = require('debug')('evolvus-docket-server:routes:main');
 module.exports = function(router) {
 
   require('./login')(router);
 
   router.use(function(req, res, next) {
-    debug('middleware kicks in');
-    next(); // make sure we go to the next routes and don't stop here
+    // make sure we go to the next routes and don't stop here
+    next();
   });
 }
--- server.js
+++ server.js
@@ -11,11 +11,20 @@ const debug = require('debug')('evolvus-docket-server:server');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const hbsViewEngine = require('hbs')
-  .__express;
+const hbs = require('hbs');
+const hbsViewEngine = hbs.__express;
+
 
 const app = express();
 const router = express.Router();
+
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'), (err) => {
+  if (err) {
+    debug('error registering partials: ' + err);
+  } else {
+    debug('registering hbs partials');
+  }
+});
 
 app.use('/', router);
 
