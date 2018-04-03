bd469b59465dd832068eb6647e9148c74de165a5
ennovatenow <anish.thomas@evolvussolutions.com>
2018-04-03 15:33:26 +0530

    lots of changes, but page comes up now

---
+++ routes/login.js
@@ -0,0 +1,15 @@
+const debug = require('debug')('evolvus-docket-server:routes:login');
+
+module.exports = (router) => {
+
+  router.route('/login')
+    .get((req, res, next) => {
+      let page = 'pages/single';
+      debug('lets try to render page', page);
+
+      res.render(page, {
+        message: ""
+      });
+    });
+
+}
--- server.js
+++ server.js
@@ -10,14 +10,19 @@ const PORT = process.env.PORT || 8080;
 const debug = require('debug')('evolvus-docket-server:server');
 const express = require('express');
 const bodyParser = require('body-parser');
-
-
-
+const path = require('path');
+const hbsViewEngine = require('hbs')
+  .__express;
 
 const app = express();
+const router = express.Router();
 
-app.set('view engine', 'hbr');
-app.use(express.static(__dirname + '/public'));
+app.use('/', router);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', hbsViewEngine);
 
 app.use(bodyParser.urlencoded({
   extended: false,
@@ -27,7 +32,7 @@ app.use(bodyParser.json({
   limit: '50mb'
 }));
 
-
+require('./routes/main')(router);
 
 
 /*
