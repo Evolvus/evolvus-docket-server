f2c907dbeae1a3fb7c9151f7e561cd7b5fe94e93
KmKavya <kavyavshaiva@gmail.com>
2018-04-04 19:41:49 +0530

    updated code and required user module

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,12 @@
 const debug = require('debug')('evolvus-docket-server:routes:login');
+const _ = require('lodash');
+var {
+  User,
+  dbUrl
+} = require('evls-user-demo');
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./scratch');
+
 
 module.exports = (router) => {
 
@@ -10,6 +18,20 @@ module.exports = (router) => {
       res.render(page, {
         message: "",
         loggedIn: false
+      });
+    })
+    .post((req, res, next) => {
+      var body = _.pick(req.body, ['email', 'password']);
+      User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+          localStorage.setItem('token', token);
+          res.status(302).redirect('/docket');
+        });
+      }).catch((e) => {
+        res.status(401).render('pages/single', {
+          message: e,
+          loggedIn: false
+        });
       });
     });
 
--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,43 @@
 const debug = require('debug')('evolvus-docket-server:routes:register');
+var {
+  User,
+  dbUrl
+} = require('evls-user-demo');
+const _ = require('lodash');
+const mongoose = require('mongoose');
+
+mongoose.connect(dbUrl + '/Userss', (err, db) => {
+  if (err) {
+    console.log('Failed to connect to the database');
+  } else {
+    console.log('connected to mongodb');
+  }
+});
 
 module.exports = (router) => {
 
   router.route('/register')
     .post((req, res, next) => {
       // put the logic here related to registration
+      var body = _.pick(req.body, ['username', 'email', 'password']);
+      var user = new User(body);
+      user.save().then(() => {
+        user.generateAuthToken();
+      }).then((token) => {
+        res.render('pages/single', {
+          message: "Registered Successfully"
+        });
+      }).catch((e) => {
+        res.status(400).render('pages/register', {
+          message: e.message
+        });
+      });
+    })
+    .get((req, res, next) => {
+      let page = 'pages/register';
+      debug('lets try to render page', page);
+      res.render(page, {
+        message: ""
+      });
     });
-
 };
