9e5678167591667f99239420ae093d668aaa56e3
KmKavya <kavyavshaiva@gmail.com>
2018-04-24 09:50:24 +0530

    added test cases for routes

--- test/routes/api.js
+++ test/routes/api.js
@@ -17,42 +17,121 @@ chai.use(chaiHttp);
 
 var serverUrl = "http://localhost:" + PORT;
 
-describe('POST /audit', () => {
-  let server;
-  before((done) => {
-    app.on('application_started', done());
+
+describe('Testing routes', () => {
+  var id;
+  describe('POST /audit', () => {
+    let server;
+    before((done) => {
+      app.on('application_started', done());
+    });
+
+    it('valid POST should return object with same attribute values', (done) => {
+
+      let auditEvent = {
+        name: 'loginEvent',
+        createdBy: 'anisht',
+        application: 'CDA',
+        source: 'loginPage',
+        ipAddress: '127.0.0.1',
+        level: 'info',
+        status: 'SUCCESS',
+        eventDateTime: new Date().toISOString(),
+        details: '{ user: "anish" }',
+        keywords: 'login CDA',
+        keyDataAsJSON: "keydata"
+      };
+
+      chai.request(serverUrl)
+        .post('/audit')
+        .send(auditEvent)
+        .end((err, res) => {
+          if (err) {
+            debug(`error in the test ${err}`);
+            done(err);
+          } else {
+            id = res.body._id;
+            debug(`response body is ${res.body}`);
+            res.should.have.status(200);
+            res.body.should.have.property('name')
+              .eql(auditEvent.name);
+            done();
+          }
+        });
+    });
   });
 
-  it('valid POST should return object with same attribute values', (done) => {
+  describe('testing GET /audit', () => {
 
-    let auditEvent = {
-      name: 'loginEvent',
-      createdBy: 'anisht',
-      application: 'CDA',
-      source: 'loginPage',
-      ipAddress: '127.0.0.1',
-      level: 'info',
-      status: 'SUCCESS',
-      eventDateTime: new Date().toISOString(),
-      details: '{ user: "anish" }',
-      keywords: 'login CDA',
-      keyDataAsJSON: "keydata"
-    };
+    it('should return status 200', (done) => {
+      chai.request(serverUrl)
+        .get('/audit')
+        .end((err, res) => {
+          if (err) {
+            debug(`error in the test ${err}`);
+            done(err);
+          } else {
+            res.should.have.status(200);
+            done();
+          }
+        });
+    });
+  });
 
-    chai.request(serverUrl)
-      .post('/audit')
-      .send(auditEvent)
-      .end((err, res) => {
-        if (err) {
-          debug(`error in the test ${err}`);
-          done(err);
-        } else {
-          debug(`response body is ${res.body}`);
-          res.should.have.status(200);
-          res.body.should.have.property('name')
-            .eql(auditEvent.name);
-          done();
-        }
-      });
+  describe('testing GET /rowDetails', () => {
+
+    it('should return status 200', (done) => {
+      chai.request(serverUrl)
+        .get('/rowDetails')
+        .query({
+          id: id
+        })
+        .end((err, res) => {
+          if (err) {
+            debug(`error in the test ${err}`);
+            done(err);
+          } else {
+            res.should.have.status(200);
+            done();
+          }
+        });
+    });
+  });
+
+  describe('testing GET /topNoOfRecords', () => {
+
+    it('should return status 200', (done) => {
+      chai.request(serverUrl)
+        .get('/topNoOfRecords')
+        .query({
+          value: 1
+        })
+        .end((err, res) => {
+          if (err) {
+            debug(`error in the test ${err}`);
+            done(err);
+          } else {
+            res.should.have.status(200);
+            done();
+          }
+        });
+    });
+  });
+
+  describe('testing GET /getByFilter', () => {
+
+    it('should return status 200', (done) => {
+      chai.request(serverUrl)
+        .get('/getByFilter')
+        .end((err, res) => {
+          if (err) {
+            debug(`error in the test ${err}`);
+            done(err);
+          } else {
+            res.should.have.status(200);
+            done();
+          }
+        });
+    });
   });
 });
